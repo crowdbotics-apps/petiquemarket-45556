b26c9ce44f16db511d3f96bdd32901ab244b6bc4
Crowdbotics <team@crowdbotics.com>
2024-01-26 00:01:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "petiquemarket-45556",
+  url: "https://petiquemarket-45556.botics.co",
+  api: "https://petiquemarket-45556.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
